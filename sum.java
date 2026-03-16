fe921a51d43181a476e0b6e78f700dcc6d90bede
sweedal2005 <vvce23ise0216@vvce.ac.in>
2026-03-16 13:11:12 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ public static void main(String[] args) {
     int a=3;
     int b=9;
     int sum=a+b;
-    System.out.println("the sum is");
+    System.out.println("the sum sum is");
     System.out.println(""+sum);
 }
 }
